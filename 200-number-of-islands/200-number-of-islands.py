62b592224fbc7cd8e41c178451a4925c1e2b76d4
Samiul Mushfik <samiulmushfik123@gmail.com>
2022-01-30 21:05:59 +0600

    Time: 398 ms (46.79%), Space: 16.4 MB (98.83%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -4,30 +4,56 @@ class LandInfo:
 
 
 class Solution:
+#     Sol#1
+#     def numIslands(self, grid: List[List[str]]) -> int:
+#         visited = [[False for i in row]for row in grid]
+#         numberOfIslands = 0
+#         for i in range(len(grid)):
+#             for j in range(len(grid[i])):
+#                 landInfo = LandInfo(0)
+#                 self.traverse(i, j, visited, grid, landInfo)
+#                 if landInfo.currentSize > 0:
+#                     numberOfIslands += 1
+#         return numberOfIslands
+    
+    
+#     def traverse(self, i, j, visited, grid, landInfo):
+#         if i < 0 or i >= len(grid):
+#             return
+#         if j < 0 or j >= len(grid[i]):
+#             return
+#         if visited[i][j]:
+#             return
+#         visited[i][j] = True
+#         if grid[i][j] is "0":
+#             return
+#         landInfo.currentSize += 1
+#         self.traverse(i+1, j, visited, grid, landInfo)
+#         self.traverse(i-1, j, visited, grid, landInfo)
+#         self.traverse(i, j-1, visited, grid, landInfo)
+#         self.traverse(i, j+1, visited, grid, landInfo)
+
     def numIslands(self, grid: List[List[str]]) -> int:
-        visited = [[False for i in row]for row in grid]
         numberOfIslands = 0
         for i in range(len(grid)):
             for j in range(len(grid[i])):
                 landInfo = LandInfo(0)
-                self.traverse(i, j, visited, grid, landInfo)
+                self.traverse(i, j, grid, landInfo)
                 if landInfo.currentSize > 0:
                     numberOfIslands += 1
         return numberOfIslands
     
     
-    def traverse(self, i, j, visited, grid, landInfo):
+    def traverse(self, i, j, grid, landInfo):
         if i < 0 or i >= len(grid):
             return
         if j < 0 or j >= len(grid[i]):
             return
-        if visited[i][j]:
-            return
-        visited[i][j] = True
         if grid[i][j] is "0":
             return
+        grid[i][j] = "0"
         landInfo.currentSize += 1
-        self.traverse(i+1, j, visited, grid, landInfo)
-        self.traverse(i-1, j, visited, grid, landInfo)
-        self.traverse(i, j-1, visited, grid, landInfo)
-        self.traverse(i, j+1, visited, grid, landInfo)
+        self.traverse(i+1, j, grid, landInfo)
+        self.traverse(i-1, j, grid, landInfo)
+        self.traverse(i, j-1, grid, landInfo)
+        self.traverse(i, j+1, grid, landInfo)
