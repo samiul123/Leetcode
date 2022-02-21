0ba4887594c796b70332368036bf4fb4ddfea258
Samiul Mushfik <samiulmushfik123@gmail.com>
2022-02-21 16:25:47 +0600

    Time: 1001 ms (54.08%), Space: 46.9 MB (41.92%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.py
+++ 234-palindrome-linked-list/234-palindrome-linked-list.py
@@ -7,47 +7,64 @@ class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         if not head.next:
             return True
-        
-        length = self.getLength(head)
-        
-        secondHalf = head
-        currIdx = 1
-        while currIdx <= length // 2:
-            secondHalf = secondHalf.next
-            currIdx += 1
-                
-        if length % 2 == 1: 
-            secondHalf = secondHalf.next
-            
-        
-        currIdx = 2
-        curr = head.next
-        temp = head
-        
-        while currIdx <= length // 2:
-            if curr:
-                temp.next = curr.next
-                curr.next = head
-                head = curr
-                currIdx += 1
-                curr = temp.next
-
-
+        stack = []
         curr = head
-        while secondHalf:
-            if curr.val != secondHalf.val:
+        
+        while curr:
+            stack.append(curr)
+            curr = curr.next
+            
+        curr = head
+        while curr:
+            if curr.val != stack.pop().val:
                 return False
             curr = curr.next
-            secondHalf = secondHalf.next
-        
         return True
         
+#         4 pass
+#         if not head.next:
+#             return True
         
-    def getLength(self, head):
-        length = 0
-        temp = head
-        while temp:
-            length += 1
-            temp = temp.next
-        return length
+#         length = self.getLength(head)
+        
+#         secondHalf = head
+#         currIdx = 1
+#         while currIdx <= length // 2:
+#             secondHalf = secondHalf.next
+#             currIdx += 1
+                
+#         if length % 2 == 1: 
+#             secondHalf = secondHalf.next
+            
+        
+#         currIdx = 2
+#         curr = head.next
+#         temp = head
+        
+#         while currIdx <= length // 2:
+#             if curr:
+#                 temp.next = curr.next
+#                 curr.next = head
+#                 head = curr
+#                 currIdx += 1
+#                 curr = temp.next
+
+
+#         curr = head
+#         while secondHalf:
+#             if curr.val != secondHalf.val:
+#                 return False
+#             curr = curr.next
+#             secondHalf = secondHalf.next
+        
+#         return True
+        
+        
+#     def getLength(self, head):
+#         length = 0
+#         temp = head
+#         while temp:
+#             length += 1
+#             temp = temp.next
+#         return length
         
