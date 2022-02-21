643c15bf0294e8e3ab0ecda87babf7bb3c9b7f08
Samiul Mushfik <samiulmushfik123@gmail.com>
2022-02-21 15:49:48 +0600

    Time: 1123 ms (39.75%), Space: 39.1 MB (83.64%) - LeetHub

---
+++ 234-palindrome-linked-list/234-palindrome-linked-list.py
@@ -0,0 +1,53 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        if not head.next:
+            return True
+        
+        length = self.getLength(head)
+        
+        secondHalf = head
+        currIdx = 1
+        while currIdx <= length // 2:
+            secondHalf = secondHalf.next
+            currIdx += 1
+                
+        if length % 2 == 1: 
+            secondHalf = secondHalf.next
+            
+        
+        currIdx = 2
+        curr = head.next
+        temp = head
+        
+        while currIdx <= length // 2:
+            if curr:
+                temp.next = curr.next
+                curr.next = head
+                head = curr
+                currIdx += 1
+                curr = temp.next
+
+
+        curr = head
+        while secondHalf:
+            if curr.val != secondHalf.val:
+                return False
+            curr = curr.next
+            secondHalf = secondHalf.next
+        
+        return True
+        
+        
+    def getLength(self, head):
+        length = 0
+        temp = head
+        while temp:
+            length += 1
+            temp = temp.next
+        return length
+        
