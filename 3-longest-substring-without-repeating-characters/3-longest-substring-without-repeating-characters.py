aec3ce0dfedf45e3808477d8c4eb088ae5dee8a3
Samiul Mushfik <samiulmushfik123@gmail.com>
2022-04-10 23:05:29 +0600

    Time: 80 ms (69.13%), Space: 14 MB (54.03%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -0,0 +1,43 @@
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+#         O(N^3) TIme (TLE)
+#         maxLength = 0
+        
+#         def is_unique(left, right):
+#             frequency = {}
+#             for i in range(left, right+1):
+#                 if s[i] not in frequency:
+#                     frequency[s[i]] = i
+#                 else:
+#                     return False
+#             return True
+        
+#         for i in range(len(s)):
+#             for j in range(i, len(s)):
+#                 if is_unique(i, j):
+#                     maxLength = max(maxLength, j - i + 1)
+        
+#         return maxLength
+
+        chars = [0] * 128
+
+        left = right = 0
+
+        res = 0
+        while right < len(s):
+            r = s[right]
+            chars[ord(r)] += 1
+
+            while chars[ord(r)] > 1:
+                l = s[left]
+                chars[ord(l)] -= 1
+                left += 1
+            
+            res = max(res, right - left + 1)
+
+            right += 1
+        return res
+        
+        
