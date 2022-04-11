9f08793b0246273a5e79e60f6d1d4f5e1b4cdc1c
Samiul Mushfik <samiulmushfik123@gmail.com>
2022-04-11 22:29:43 +0600

    Time: 64 ms (89.15%), Space: 14 MB (54.00%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -2,6 +2,21 @@
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        charSet = set()
+        
+        left = 0
+        
+        maxLen = 0
+        
+        for right in range(len(s)):
+            while s[right] in charSet:
+                charSet.remove(s[left])
+                left += 1
+            charSet.add(s[right])
+            maxLen = max(maxLen, right - left + 1)
+        
+        return maxLen
+        
 #         O(N^3) TIme (TLE)
 #         maxLength = 0
         
@@ -21,43 +36,33 @@ class Solution:
         
 #         return maxLength
         
-        dic = {}
+#         dic = {}
         
-        def increaseCharCount(char):
-            if char in dic:
-                dic[char] += 1
-            else:
-                dic[char] = 1
-            
-        def decreaseCharCount(char):
-            dic[char] -= 1
-        
-        def getCount(char):
-            return dic[char]
-
-        # chars = [0] * 128
+#         # chars = [0] * 128
         
 
-        left = right = 0
+#         left = right = 0
 
-        res = 0
-        while right < len(s):
-            r = s[right]
+#         res = 0
+#         while right < len(s):
+#             r = s[right]
             
-            # chars[ord(r)] += 1
+#             # chars[ord(r)] += 1
             
-            # increaseCharCount(r)
-            dic[r] = 1 if r not in dic else dic[r] + 1
+#             dic[r] = 1 if r not in dic else dic[r] + 1
 
-            while dic[r] > 1:
-                l = s[left]
-                # chars[ord(l)] -= 1
-                dic[l] -= 1
-                left += 1
+#             while dic[r] > 1:
+#                 l = s[left]
+#                 # chars[ord(l)] -= 1
+#                 dic[l] -= 1
+#                 left += 1
             
-            res = max(res, right - left + 1)
+#             res = max(res, right - left + 1)
 
-            right += 1
-        return res
+#             right += 1
+#         return res
+
+        
+    
         
         
