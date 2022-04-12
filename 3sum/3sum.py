e7c7337797164d5fee9ea83fc776617be3b770c1
Samiul Mushfik <samiulmushfik123@gmail.com>
2022-04-12 21:53:57 +0600

    Time: 1928 ms, Memory: 17.3 MB - LeetHub

--- 3sum/3sum.py
+++ 3sum/3sum.py
@@ -1,27 +1,44 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        result = set()
         nums.sort()
-        triplets = set()
-        for i in range(len(nums) - 2):
-            left = i + 1
-            right = len(nums) - 1
-            
+        for index, currentNumber in enumerate(nums):
+            remaining = 0 - currentNumber
+            left = index + 1
+            right = len(nums)-1
             while left < right:
-                trip_sum = nums[i] + nums[left] + nums[right]
-                if trip_sum == 0:
-                    triplets.add((nums[i], nums[left], nums[right]))
-                
-                # If the current triplet sum is negative, or if we already have a triplet of 0 and want to try another combination
-                # advance the right pointer
-                # The left pointer will advance non decreasingly
-                # The right pointer will advance non increasingly
-                if trip_sum <= 0:
+                sumLeftRight = nums[left] + nums[right]
+                if sumLeftRight < remaining:
                     left += 1
-                else:
+                elif sumLeftRight > remaining:
                     right -= 1
+                else:
+                    result.add((currentNumber, nums[left], nums[right]))
+                    left += 1
+                    right -= 1
+        return result
+#         nums.sort()
+#         triplets = set()
+#         for i in range(len(nums) - 2):
+#             left = i + 1
+#             right = len(nums) - 1
+            
+#             while left < right:
+#                 trip_sum = nums[i] + nums[left] + nums[right]
+#                 if trip_sum == 0:
+#                     triplets.add((nums[i], nums[left], nums[right]))
+                
+#                 # If the current triplet sum is negative, or if we already have a triplet of 0 and want to try another combination
+#                 # advance the right pointer
+#                 # The left pointer will advance non decreasingly
+#                 # The right pointer will advance non increasingly
+#                 if trip_sum <= 0:
+#                     left += 1
+#                 else:
+#                     right -= 1
                     
                     
-        return list(triplets)
+#         return list(triplets)
 #         triplets = set()
 #         dups = set()
 #         numSet = {}
@@ -40,4 +57,6 @@ class Solution:
 #                         numSet[nums[j]] = j
                     
 #         return triplets
+
+
         
