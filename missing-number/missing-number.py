d73b5e7c4e83669b0d9dfd53e2b12ed360bc6771
Samiul Mushfik <samiulmushfik123@gmail.com>
2022-04-17 00:29:28 +0600

    Time: 142 ms, Memory: 15.1 MB - LeetHub

--- missing-number/missing-number.py
+++ missing-number/missing-number.py
@@ -1,11 +1,21 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        numSet = set()
+#         numSet = set()
         
+#         for i in nums:
+#             numSet.add(i)
+        
+#         for i in range(len(nums)+1):
+#             if i not in numSet:
+#                 return i
+        length = len(nums)
+        sumInRange = 0
+        for i in range(length+1):
+            sumInRange += i
+        
+        sumInNums = 0
         for i in nums:
-            numSet.add(i)
+            sumInNums += i
         
-        for i in range(len(nums)+1):
-            if i not in numSet:
-                return i
+        return sumInRange - sumInNums
         
