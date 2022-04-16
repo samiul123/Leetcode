b27b27c144824e236f2729dcb8a0ec476b240de7
Samiul Mushfik <samiulmushfik123@gmail.com>
2022-04-17 04:50:13 +0600

    Time: 98 ms, Memory: 14 MB - LeetHub

---
+++ most-common-word/most-common-word.py
@@ -0,0 +1,56 @@
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        left = 0
+        right = 0
+        
+        bannedSet = set()
+        for word in banned:
+            bannedSet.add(word)
+        
+        # specialChars = set(" ", "!", "?", "'", ",", ";", ".")
+        specialChars = set(" !?',;.")
+        wordCount = collections.defaultdict(int)
+        maxFrequency = - math.inf
+        maxFrequentWord = ""
+        
+        while right < len(paragraph):
+            currChar = paragraph[right]
+            
+            print("CurrChar: {}".format(currChar))
+            print("specialChars: ", specialChars)
+            
+            if paragraph[right] in specialChars:
+#                 PROCESS [left, right-1]
+                currWord = paragraph[left:right].lower()
+                print("Left: {}, Right: {}, CurrWord: {}".format(left, right, currWord))
+                if currWord not in bannedSet:
+                    wordCount[currWord] += 1
+                    if wordCount[currWord] > maxFrequency:
+                        maxFrequency = wordCount[currWord]
+                        maxFrequentWord = currWord
+                    print("MaxFrequency: {}, MaxFrequentWord: {}".format(maxFrequency, maxFrequentWord))
+                    
+                if currChar == ' ':
+                    right += 1
+                else:
+                    while right < len(paragraph) and paragraph[right] != " ":
+                        right += 1
+                    right += 1
+                # elif currChar == "'":
+                #     right += 3
+                # else:
+                #     right += 2
+                left = right
+            elif right == len(paragraph) - 1:
+                currWord = paragraph[left:right+1].lower()
+                print("Left: {}, Right: {}, CurrWord: {}".format(left, right, currWord))
+                if currWord not in bannedSet:
+                    wordCount[currWord] += 1
+                    if wordCount[currWord] > maxFrequency:
+                        maxFrequency = wordCount[currWord]
+                        maxFrequentWord = currWord
+                right += 1
+            else:
+                right += 1    
+        
+        return maxFrequentWord
