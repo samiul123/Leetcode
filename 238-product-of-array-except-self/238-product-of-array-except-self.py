10760b267454d3884a43b501530e53032918377d
Samiul Mushfik <samiulmushfik123@gmail.com>
2022-04-16 22:37:58 +0600

    Time: 264 ms (72.06%), Space: 21.2 MB (45.29%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -14,10 +14,7 @@ class Solution:
         for i in range(1, length):
             left[i] = left[i-1]*nums[i-1]
             right[length-i-1] = right[length-i]*nums[length-i]
-        
-        # for i in range(length-2, -1, -1):
-        #     right[i] = right[i+1]*nums[i+1]
-        
+            
         
         for i in range(length):
             answer[i] = left[i] * right[i]
