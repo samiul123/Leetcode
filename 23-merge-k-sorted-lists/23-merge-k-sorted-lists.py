a8ed1bd32caa42cc8cc11a14b7dfdedc10fa2e8d
Samiul Mushfik <samiulmushfik123@gmail.com>
2022-04-24 04:28:47 +0600

    Time: 116 ms (78.02%), Space: 17.7 MB (76.62%) - LeetHub

---
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -0,0 +1,49 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+#         divide and conquer
+        if len(lists) == 0:
+            return None
+        length = len(lists)
+        step = 1
+        while step < length:
+            for i in range(0, length - step, step*2):
+                lists[i] = self.getMergedHead(lists[i], lists[i+step])
+            step = step*2
+        return lists[0]
+        
+        
+#         MERGE one by one
+#         if len(lists) == 0:
+#             return None
+        
+#         mergedHead = lists[0]
+        
+#         for i in range(1, len(lists)):
+#             head2 = lists[i]
+#             mergedHead = self.getMergedHead(mergedHead, head2)
+            
+#         return mergedHead
+    
+    
+    def getMergedHead(self, head1, head2):
+        mergedHead = ListNode(0)
+        prev = mergedHead
+        
+        temp1 = head1
+        temp2 = head2
+        
+        while temp1 and temp2:
+            if temp1.val <= temp2.val:
+                prev.next = temp1
+                temp1 = temp1.next
+            else:
+                prev.next = temp2
+                temp2 = temp2.next
+            prev = prev.next
+        prev.next = temp1 or temp2
+        return mergedHead.next
