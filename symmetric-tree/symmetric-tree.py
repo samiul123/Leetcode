68df3f96be0f23b22204005711f1557132cc5920
Samiul Mushfik <samiulmushfik123@gmail.com>
2022-05-15 22:06:21 +0600

    Time: 55 ms, Memory: 14 MB - LeetHub

--- symmetric-tree/symmetric-tree.py
+++ symmetric-tree/symmetric-tree.py
@@ -4,49 +4,20 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-PREORDER = 0
-MODIFIED_PREORDER = 1
-INVALID = -999
-
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        return self.isSymmetricHelper(root.left, root.right)
     
-        if root.left and root.right:
-            leftTraversal = []
-            rightTraversal = []
-            self.getTraversal(root.left, PREORDER, leftTraversal)
-            self.getTraversal(root.right, MODIFIED_PREORDER, rightTraversal)
-            
-            return self.areEqual(leftTraversal, rightTraversal)
-        
-        elif root and not root.left and not root.right:
+    
+    def isSymmetricHelper(self, left, right):
+        if not left and not right:
             return True
         
-        return False
-    
-    
-    def areEqual(self, leftTraversal, rightTraversal):
-        if len(leftTraversal) != len(rightTraversal):
+        if not left or not right:
             return False
-            
-        for i in range(len(leftTraversal)):
-            if leftTraversal[i] != rightTraversal[i]:
-                return False 
-
-        return True
+        
+        isSymmetric = left.val == right.val
+        
+        return isSymmetric and self.isSymmetricHelper(left.left, right.right) and self.isSymmetricHelper(left.right, right.left)
     
     
-    def getTraversal(self, node, type, traversal):
-        if not node:
-            traversal.append(INVALID)
-            return
-        
-        traversal.append(node.val)
-        if type == PREORDER:
-            self.getTraversal(node.left, type, traversal)
-            self.getTraversal(node.right, type, traversal)
-        
-        else:
-            self.getTraversal(node.right, type, traversal)
-            self.getTraversal(node.left, type, traversal)
