c3236830bb7f06bbb6dbf0818932bedbf49ead6c
Samiul Mushfik <samiulmushfik123@gmail.com>
2022-05-23 23:07:04 +0600

    Time: 416 ms (50.39%), Space: 16.4 MB (63.16%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,109 +1,30 @@
-class LandInfo:
-	def __init__(self, currentSize):
-		self.currentSize = currentSize;
-
-
 class Solution:
-#     Sol#1 (DFS)
-#     def numIslands(self, grid: List[List[str]]) -> int:
-#         visited = [[False for i in row]for row in grid]
-#         numberOfIslands = 0
-#         for i in range(len(grid)):
-#             for j in range(len(grid[i])):
-#                 landInfo = LandInfo(0)
-#                 self.traverse(i, j, visited, grid, landInfo)
-#                 if landInfo.currentSize > 0:
-#                     numberOfIslands += 1
-#         return numberOfIslands
-    
-    
-#     def traverse(self, i, j, visited, grid, landInfo):
-#         if i < 0 or i >= len(grid):
-#             return
-#         if j < 0 or j >= len(grid[i]):
-#             return
-#         if visited[i][j]:
-#             return
-#         visited[i][j] = True
-#         if grid[i][j] is "0":
-#             return
-#         landInfo.currentSize += 1
-#         self.traverse(i+1, j, visited, grid, landInfo)
-#         self.traverse(i-1, j, visited, grid, landInfo)
-#         self.traverse(i, j-1, visited, grid, landInfo)
-#         self.traverse(i, j+1, visited, grid, landInfo)
-# 
-#     Sol#2 (DFS-without visited array)
-#     def numIslands(self, grid: List[List[str]]) -> int:
-#         numberOfIslands = 0
-#         for i in range(len(grid)):
-#             for j in range(len(grid[i])):
-#                 landInfo = LandInfo(0)
-#                 self.traverse(i, j, grid, landInfo)
-#                 if landInfo.currentSize > 0:
-#                     numberOfIslands += 1
-#         return numberOfIslands
-    
-    
-#     def traverse(self, i, j, grid, landInfo):
-#         if i < 0 or i >= len(grid):
-#             return
-#         if j < 0 or j >= len(grid[i]):
-#             return
-#         if grid[i][j] is "0":
-#             return
-#         grid[i][j] = "0"
-#         landInfo.currentSize += 1
-#         self.traverse(i+1, j, grid, landInfo)
-#         self.traverse(i-1, j, grid, landInfo)
-#         self.traverse(i, j-1, grid, landInfo)
-#         self.traverse(i, j+1, grid, landInfo)
-# 
-#     Sol#3 (DFS - optimizaztion in recursive call)
-#     def numIslands(self, grid: List[List[str]]) -> int:
-#         numberOfIslands = 0
-#         for i in range(len(grid)):
-#             for j in range(len(grid[i])):
-#                 if grid[i][j] is "1":
-#                     self.traverse(i, j, grid)
-#                     numberOfIslands += 1
-#         return numberOfIslands
-#    
-#    
-#     def traverse(self, i, j, grid):
-#         grid[i][j] = "0"
-#         if i+1 < len(grid) and grid[i+1][j] is "1":
-#             self.traverse(i+1, j, grid)
-#         if i-1 >= 0 and grid[i-1][j] is "1":
-#             self.traverse(i-1, j, grid)
-#         if j-1 >= 0 and grid[i][j-1] is "1":
-#             self.traverse(i, j-1, grid)
-#         if j+1 < len(grid[i]) and grid[i][j+1] is "1":
-#             self.traverse(i, j+1, grid)
-#    
-#   Sol#4 (BFS)
     def numIslands(self, grid: List[List[str]]) -> int:
-        numberOfIslands = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[i])):
-                if grid[i][j] is "1":
-                    self.traverse(i, j, grid)
-                    numberOfIslands += 1
-        return numberOfIslands
-   
-
-    def traverse(self, i, j, grid):
-        queue = [(i, j)]
-        grid[i][j] = "0"
-        while len(queue) > 0:
-            current_i, current_j = queue.pop(0)
+        numIslands = 0
         
-            for next_i, next_j in [(current_i+1, current_j), (current_i-1, current_j), (current_i, current_j-1), (current_i, current_j + 1)]:
-                if next_i < 0 or next_i >= len(grid) or next_j < 0 or next_j >= len(grid[0]):
-                    continue
-                    
-                if grid[next_i][next_j] is "1":
-                    grid[next_i][next_j] = "0"
-                    queue.append((next_i, next_j))
-            
+        visited = [[False for col in range(len(grid[row]))]for row in range(len(grid))]
+        
+        for row in range(len(grid)):
+            for col in range(len(grid[row])):
+                if grid[row][col] == '1' and not visited[row][col]:
+                    self.traverse(row, col, grid, visited)
+                    numIslands += 1
+        return numIslands
+        
     
+    def traverse(self, row, col, grid, visited):
+        if row < 0 or row >= len(grid) or col < 0 or col >= len(grid[row]):
+            return
+        
+        if grid[row][col] == '0':
+            return
+        
+        if visited[row][col]:
+            return
+        
+        visited[row][col] = True
+                                                                            
+        self.traverse(row+1, col, grid, visited)
+        self.traverse(row-1, col, grid, visited)
+        self.traverse(row, col+1, grid, visited)
+        self.traverse(row, col-1, grid, visited)
