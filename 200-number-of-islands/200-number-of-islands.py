55718df42a770f18838bc456a23e227646c1b3fb
Samiul Mushfik <samiulmushfik123@gmail.com>
2022-05-23 23:08:50 +0600

    Time: 510 ms (25.75%), Space: 16.5 MB (46.45%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -2,29 +2,24 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         numIslands = 0
         
-        visited = [[False for col in range(len(grid[row]))]for row in range(len(grid))]
-        
         for row in range(len(grid)):
             for col in range(len(grid[row])):
-                if grid[row][col] == '1' and not visited[row][col]:
-                    self.traverse(row, col, grid, visited)
+                if grid[row][col] == '1':
+                    self.traverse(row, col, grid)
                     numIslands += 1
         return numIslands
         
     
-    def traverse(self, row, col, grid, visited):
+    def traverse(self, row, col, grid):
         if row < 0 or row >= len(grid) or col < 0 or col >= len(grid[row]):
             return
         
         if grid[row][col] == '0':
             return
         
-        if visited[row][col]:
-            return
-        
-        visited[row][col] = True
+        grid[row][col] = '0'
                                                                             
-        self.traverse(row+1, col, grid, visited)
-        self.traverse(row-1, col, grid, visited)
-        self.traverse(row, col+1, grid, visited)
-        self.traverse(row, col-1, grid, visited)
+        self.traverse(row+1, col, grid)
+        self.traverse(row-1, col, grid)
+        self.traverse(row, col+1, grid)
+        self.traverse(row, col-1, grid)
