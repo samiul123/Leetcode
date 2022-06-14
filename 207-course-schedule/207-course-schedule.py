2fada4860affc0a5344eaf6211148c00e239dc58
Samiul Mushfik <samiulmushfik123@gmail.com>
2022-06-14 20:59:41 +0600

    Time: 132 ms (56.48%), Space: 16.7 MB (56.26%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,39 +1,62 @@
+INITIAL = 0
+IN_PROGRESS = 1
+DONE = 2
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
 #         build the graph
-        
+    
         dependencyMap = collections.defaultdict(list)
         
         for prerequisite in prerequisites:
             course, prevCourse = prerequisite[0], prerequisite[1]
             dependencyMap[prevCourse].append(course)
             
-        checked = [False] * numCourses
-        path = [False] * numCourses
+        status = [INITIAL] * numCourses
+        
         for course in range(numCourses):
-            if self.isCyclic(course, dependencyMap, path, checked):
-                return False
+            if status[course] == INITIAL:
+                if self.isCyclic(course, dependencyMap, status):
+                    return False
         return True
     
-    def isCyclic(self, currCourse, dependencyMap, path, checked):
-        if checked[currCourse]:
-            return False
+    
+    def isCyclic(self, currCourse, dependencyMap, status):
+        status[currCourse] = IN_PROGRESS
         
-        if path[currCourse]:
-            return True
-        
-        path[currCourse] = True
-        
-        isCyclic = False
         for child in dependencyMap[currCourse]:
-            isCyclic = self.isCyclic(child, dependencyMap, path, checked)
-            if isCyclic:
-                break
+            if status[child] == IN_PROGRESS:
+                return True
+            if status[child] == INITIAL and self.isCyclic(child, dependencyMap, status):
+                return True
         
-        path[currCourse] = False
+        status[currCourse] = DONE
+        return False
+#         checked = [False] * numCourses
+#         path = [False] * numCourses
+#         for course in range(numCourses):
+#             if self.isCyclic(course, dependencyMap, path, checked):
+#                 return False
+#         return True
+    
+#     def isCyclic(self, currCourse, dependencyMap, path, checked):
+#         if checked[currCourse]:
+#             return False
         
-        checked[currCourse] = True
+#         if path[currCourse]:
+#             return True
         
-        return isCyclic
+#         path[currCourse] = True
+        
+#         isCyclic = False
+#         for child in dependencyMap[currCourse]:
+#             isCyclic = self.isCyclic(child, dependencyMap, path, checked)
+#             if isCyclic:
+#                 break
+        
+#         path[currCourse] = False
+        
+#         checked[currCourse] = True
+        
+#         return isCyclic
                 
             
