9e5e3fc5e5740557c8bc0d712084321d2cc74dbb
Samiul Mushfik <samiulmushfik123@gmail.com>
2022-07-06 00:53:26 +0600

    Time: 72 ms (16.80%), Space: 14.2 MB (76.81%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -0,0 +1,22 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        open, close = 0, 0
+        combinations = []
+        
+        def recurse(open, close, combination):
+            if open == n and close == n:
+                combinations.append("".join(combination))
+                return
+            
+            if open < n:
+                combination.append("(")
+                recurse(open+1, close, combination)
+                combination.pop()
+            if close < open:
+                combination.append(")")
+                recurse(open, close+1, combination)
+                combination.pop()
+                
+        recurse(open, close, [])
+        return combinations
+        
