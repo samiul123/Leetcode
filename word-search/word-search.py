460fb2685d60d0b23ab61013fdc8d89077b618d2
Samiul Mushfik <samiulmushfik123@gmail.com>
2022-07-08 21:23:25 +0600

    Time: 7330 ms, Memory: 13.9 MB - LeetHub

---
+++ word-search/word-search.py
@@ -0,0 +1,45 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        n_row = len(board)
+        n_col = len(board[0])
+        
+        if n_row*n_col < len(word):
+            return False
+        
+        visited = [[False for col in range(n_col)] for row in range(n_row)]
+        search_idx = 0
+        
+        for i in range(n_row):
+            for j in range(n_col):
+                if board[i][j] != word[search_idx]:
+                    continue
+                if self.traverse(board, i, j, word, search_idx, visited):
+                    return True
+        return False
+    
+    
+    def traverse(self, board, row, col, word, search_idx, visited):
+        if row < 0 or row >= len(board) or col < 0 or col >= len(board[row] or search_idx >= len(word)):
+            return False
+        
+        if board[row][col] != word[search_idx]:
+            return False
+        
+        if visited[row][col]:
+            return False
+        
+        visited[row][col] = True
+        
+        if search_idx == len(word) - 1:
+            return True
+        
+        if self.traverse(board, row+1, col, word, search_idx + 1, visited):
+            return True
+        if self.traverse(board, row-1, col, word, search_idx + 1, visited):
+            return True
+        if self.traverse(board, row, col+1, word, search_idx + 1, visited):
+            return True
+        if self.traverse(board, row, col-1, word, search_idx + 1, visited):
+            return True
+        
+        visited[row][col] = False
