d66ba4fe44f67c2d6fa6e8c4bf4d3b55ac8a0cf8
Samiul Mushfik <samiulmushfik123@gmail.com>
2022-08-21 23:28:51 +0600

    Time: 934 ms (67.82%), Space: 13.9 MB (63.98%) - LeetHub

--- 18-4sum/18-4sum.py
+++ 18-4sum/18-4sum.py
@@ -7,6 +7,8 @@
 # else: right --
 
 
+# [-5,-4,-3,-2,1,3,3,5] target = -11
+# 
 
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
@@ -14,26 +16,35 @@ class Solution:
         result = set()
         
         for i in range(len(nums) - 3):
-            for j in range(i+1, len(nums) - 2):
-                left, right = j+1, len(nums) - 1
-                
-                while left < right:
-                    sum = nums[i] + nums[j] + nums[left] + nums[right]
-                    
-                    if sum == target:
-                        result.add((nums[i], nums[j], nums[left], nums[right]))
-                        left += 1
-                        right -= 1
-                        while left < right and nums[left] == nums[left-1]:
-                            left += 1
-                        
-                    
-                    elif sum < target:
-                        left += 1
-                    
-                    else:
-                        right -= 1
+            if i == 0 or nums[i] != nums[i-1]:
+                self.threeSum(nums, i, result, target - nums[i])
         
         return result
-                
+    
+    
+    def threeSum(self, nums: List[int], i: int, result: List[List[int]], target: int):
+        for j in range(i+1, len(nums) - 2):
+            if j == i+1 or nums[j] != nums[j-1]:
+                self.twoSum(nums, i, j, result, target - nums[j])
+            
+    
+    def twoSum(self, nums: List[int], i: int, j: int, result: List[List[int]], target: int):
+        left, right = j+1, len(nums) - 1
+
+        while left < right:
+            sum = nums[left] + nums[right]
+
+            if sum == target:
+                result.add((nums[i], nums[j], nums[left], nums[right]))
+                left += 1
+                right -= 1
+                while left < right and nums[left] == nums[left-1]:
+                    left += 1
+
+            elif sum < target:
+                left += 1
+
+            else:
+                right -= 1
+            
         
