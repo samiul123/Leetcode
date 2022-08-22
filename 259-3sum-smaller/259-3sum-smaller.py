3bd34f947c7f8becbed6475b622613311299d218
Samiul Mushfik <samiulmushfik123@gmail.com>
2022-08-22 20:48:48 +0600

    Time: 1450 ms (25.91%), Space: 13.9 MB (91.74%) - LeetHub

--- 259-3sum-smaller/259-3sum-smaller.py
+++ 259-3sum-smaller/259-3sum-smaller.py
@@ -1,3 +1,11 @@
+# The largest num + smallest < key, so we have 2 choices: 1. move right down, all elements are satisfied since r is the current largest one; => all the sum btw l and r is r-l.
+# 2. move left up, we need to check if it is satisfied.
+
+# for example,  we have -21, -3, -2, 2, 4,  5   target=2; 
+# For the 1st nums[0] = -21, => key = 2+21 = 23
+#     -3+5<23 ==> we know -3+5, -3+4, -3+2 and -3+-2 must be valid. That's why sum+= (hi-lo);
+# Then move lo++, to check if ( (lo++) + hi) still < key.
+
 class Solution:
     def threeSumSmaller(self, nums: List[int], target: int) -> int:
         count = 0
@@ -13,7 +21,7 @@ class Solution:
                     # IMPORTANT
                     # TRICKY PART HERE
                     # IF THREESUM < TARGET, THEN BECAUSE THEE ARRAY IS SORTED
-                    # ALL NUMBERS IN BETWEEN WILL ALSO BE LESS OR EQUAL TO K
+                    # ALL NUMBERS IN BETWEEN WILL ALSO BE LESS OR EQUAL TO right
                     # AND THEREFORE BE VALID ANSWERS
                     count += right - left
                     left += 1
