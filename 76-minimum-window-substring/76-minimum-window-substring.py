2d57a795c84411866efb2a61c85c87526740b553
Samiul Mushfik <samiulmushfik123@gmail.com>
2022-08-24 23:33:27 +0600

    Time: 192 ms (41.17%), Space: 14.8 MB (36.91%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -0,0 +1,42 @@
+# frequency_t -> store frequency of letters in t
+# have to maintain a window in s which contains letters as many as in t
+# if window maintains this property while being shrinked, have to update 
+#    the minWindow size
+# Otherwise, expand the window
+# finally return the substring which corresponds to the minWindow size
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        char_t_frequency = collections.defaultdict(int)
+        for char in t:
+            char_t_frequency[char] += 1
+        
+        left, right = 0, 0
+        # (size, left, right)
+        min_window = (math.inf, 0, 0)
+        char_window_frequency = collections.defaultdict(int)
+        formation_progress = 0
+        
+        while right < len(s):
+            if s[right] in char_t_frequency:
+                char_window_frequency[s[right]] += 1
+                
+                if char_window_frequency[s[right]] == char_t_frequency[s[right]]:
+                    formation_progress += 1
+                    
+            while left <= right and formation_progress == len(char_t_frequency):
+                if s[left] in char_window_frequency:
+                    current_window_size = right - left + 1
+                    if current_window_size < min_window[0]:
+                        min_window = (current_window_size, left, right)
+
+                    char_window_frequency[s[left]] -= 1
+
+                    if char_window_frequency[s[left]] < char_t_frequency[s[left]]:
+                        formation_progress -= 1
+                left += 1
+            right += 1
+        
+        return "" if min_window[0] == math.inf else s[min_window[1]:min_window[2]+1]
+                
+                
