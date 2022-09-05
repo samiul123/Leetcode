af742ef7c645c6974e59331a2b09f7d820a1067b
Samiul Mushfik <samiulmushfik123@gmail.com>
2022-09-05 14:08:03 +0600

    Time: 3148 ms (8.52%), Space: 36.1 MB (44.02%) - LeetHub

---
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.py
@@ -0,0 +1,64 @@
+class MedianFinder:
+
+    def __init__(self):
+        self.store = []
+        
+    def addNum(self, num: int) -> None:
+        # if len(self.store) == 0:
+        #     self.store.append(num)
+        # else:
+        self.insert(num)
+        
+    def findMedian(self) -> float:
+        length = len(self.store)
+        
+        return self.store[length//2] if length % 2 else (self.store[length//2 - 1] + self.store[length//2]) * .5
+    
+    def insert(self, num):
+        left, right = 0, len(self.store) - 1
+        positionToInsert = -1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            
+            if self.store[mid] > num:
+                right = mid - 1
+            
+            elif self.store[mid] < num:
+                left = mid + 1
+            else:
+                positionToInsert = mid
+                break
+        
+        if positionToInsert == -1:
+            positionToInsert = left
+        
+        self.store.insert(positionToInsert, num)
+        
+        
+
+# Sort Everytime while reading
+# Space - O(n)
+
+# class MedianFinder:
+
+#     def __init__(self):
+#         self.store = []
+        
+# # O(1) - Amortized
+#     def addNum(self, num: int) -> None:
+#         self.store.append(num)
+        
+# # O(nlogn) - Time
+#     def findMedian(self) -> float:
+#         self.store.sort()
+#         length = len(self.store)
+        
+#         return self.store[length//2] if length % 2 else (self.store[length//2 - 1] + self.store[length//2]) * .5
+        
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
